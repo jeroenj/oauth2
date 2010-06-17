4c8b59a3d2130ed130f5966a575a28f3c6b30bfb
Jeroen Jacobs <jacobsjeroen@gmail.com>
2010-06-17 15:52:11 +0200

    Support for refresh_token

--- lib/oauth2/strategy/web_server.rb
+++ lib/oauth2/strategy/web_server.rb
@@ -6,31 +6,47 @@ module OAuth2
       def authorize_params(options = {}) #:nodoc:
         super(options).merge('type' => 'web_server')
       end
-      
+
       # Retrieve an access token given the specified validation code.
       # Note that you must also provide a <tt>:redirect_uri</tt> option
       # in order to successfully verify your request for most OAuth 2.0
       # endpoints.
       def get_access_token(code, options = {})
         response = @client.request(:post, @client.access_token_url, access_token_params(code, options))
-        params   = Yajl::Parser.parse(response) rescue Rack::Utils.parse_query(response)
-        access   = params['access_token']
-        expires_in = params['expires_in']
-        refresh  = params['refresh_token']
-        OAuth2::AccessToken.new(@client, access, refresh)
+        parse_response(response)
       end
-      
+
+      def refresh_access_token(code, options = {})
+        response = @client.request(:post, @client.access_token_url, refresh_token_params(code, options))
+        parse_response(response)
+      end
+
       # <b>DEPRECATED:</b> Use #get_access_token instead.
       def access_token(*args)
         warn '[DEPRECATED] OAuth2::Strategy::WebServer#access_token is deprecated, use #get_access_token instead. Will be removed in 0.1.0'
         get_access_token(*args)
       end
-      
+
       def access_token_params(code, options = {}) #:nodoc:
         super(options).merge({
           :grant_type => 'authorization_code',
           :code => code
         })
+      end
+
+      def access_token_params(code, options = {}) #:nodoc:
+        super(options).merge({
+          :grant_type => 'refresh_token',
+          :code => code
+        })
+      end
+
+      def parse_response(response)
+        params   = Yajl::Parser.parse(response) rescue Rack::Utils.parse_query(response)
+        access   = params['access_token']
+        expires_in = params['expires_in']
+        refresh  = params['refresh_token']
+        OAuth2::AccessToken.new(@client, access, refresh)        
       end
     end
   end
